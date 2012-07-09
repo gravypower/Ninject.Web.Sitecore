ec931a4793b9c13dbca574eb2e97236db106c2c5
Aaron Job <awjob42@gmail.com>
2012-07-10 09:02:10 +1000

    added base for http request processor
    As Sitecore keeps processors around in memory you need to ask ninject
    for injections each time it runs.

---
+++ src/Ninject.Web.Sitecore/HttpRequestProcessorBase.cs
@@ -0,0 +1,32 @@
+﻿
+using Sitecore.Pipelines.HttpRequest;
+namespace Ninject.Web.Sitecore
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class HttpRequestProcessorBase :  HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            KernelContainer.Inject(this);
+
+            foreach (var property in GetType().GetProperties())
+            {
+                var attribute = property.Attributes.GetType();
+                if (attribute == typeof(InjectAttribute))
+                {
+                    var injectedProp = KernelContainer.Kernel.Get(property.GetType());
+                    //var rightValue = property.GetValue(injectedProp, null);
+
+                    //set the properties values for the right to the left object
+                    property.SetValue(injectedProp, injectedProp, null);
+                }
+            }
+            
+            DoProcessRequest(args);
+        }
+
+        protected abstract void DoProcessRequest(HttpRequestArgs args);
+    }
+}
